7ccbd83030d2b76eec5e09a0519187e79e28d46a
Michelepal <Mikaelquasimodus@gmail.com>
2022-04-29 10:11:45 +0200

    fix

--- backend/src/main/java/controller/RestApi.java
+++ backend/src/main/java/controller/RestApi.java
@@ -3,20 +3,15 @@ package controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import model.Studente;
-import repository.Repo;
 import services.StudentiDB;
 
 
-@Controller
+@RestController
 
 public class RestApi {
 	
