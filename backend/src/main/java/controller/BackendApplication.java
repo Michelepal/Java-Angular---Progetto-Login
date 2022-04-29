fa8fc45d3c24650b325dc6b0c5603f7feb934d19
Michelepal <Mikaelquasimodus@gmail.com>
2022-04-29 10:20:43 +0200

    fix source package

--- backend/src/main/java/controller/BackendApplication.java
+++ backend/src/main/java/controller/BackendApplication.java
@@ -4,7 +4,7 @@ package controller;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages= {"src.main.java"})
 public class BackendApplication {
 
 	public static void main(String[] args) {
